757cf44445c29e1686965461126818b8b1a50692
Yeshope <108490526+Yeshope@users.noreply.github.com>
2023-06-27 09:04:56 +0700

    up popup

--- wp-login.php
+++ wp-login.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<html lang="vi">
+<html lang="en-GB">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Đăng nhập &lsaquo; doancuoiky &#8212; WordPress</title>
+	<title>Log In &lsaquo; doancuoiky &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="stylesheet" id="dashicons-css" href="https://yeshope.github.io/doancuoiki/wp-includes/css/dashicons.min.css?ver=6.2.2" media="all">
 <link rel="stylesheet" id="buttons-css" href="https://yeshope.github.io/doancuoiki/wp-includes/css/buttons.min.css?ver=6.2.2" media="all">
@@ -12,41 +12,41 @@
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
-	<body class="login no-js login-action-login wp-core-ui  locale-vi">
+	<body class="login no-js login-action-login wp-core-ui  locale-en-gb">
 	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
 		<div id="login">
-		<h1><a href="https://vi.wordpress.org/">Xây dựng bằng WordPress</a></h1>
+		<h1><a href="https://en-gb.wordpress.org/">Powered by WordPress</a></h1>
 	
 		<form name="loginform" id="loginform" action="http://localhost/doancuoiki/wp-login.php" method="post">
 			<p>
-				<label for="user_login">Tên người dùng hoặc Địa chỉ Email</label>
+				<label for="user_login">Username or Email Address</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username">
 			</p>
 
 			<div class="user-pass-wrap">
-				<label for="user_pass">Mật khẩu</label>
+				<label for="user_pass">Password</label>
 				<div class="wp-pwd">
 					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false">
-					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Hiện mật khẩu">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
 						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
 					</button>
 				</div>
 			</div>
-						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Tự động đăng nhập</label></p>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
-				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Đăng nhập">
-									<input type="hidden" name="redirect_to" value="http://localhost/doancuoiki/wp-admin/?simply_static_page=47">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
+									<input type="hidden" name="redirect_to" value="http://localhost/doancuoiki/wp-admin/?simply_static_page=800">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-				<a href="https://yeshope.github.io/doancuoiki/my-account/lost-password/">Bạn quên mật khẩu?</a>			</p>
+				<a href="https://yeshope.github.io/doancuoiki/my-account/lost-password/">Lost your password?</a>			</p>
 					<script type="text/javascript">function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 				<p id="backtoblog">
-			<a href="https://yeshope.github.io/doancuoiki/">&larr; Quay lại doancuoiky</a>		</p>
+			<a href="https://yeshope.github.io/doancuoiki/">&larr; Go to doancuoiky</a>		</p>
 			</div>
 				<div class="language-switcher">
 				<form id="language-switcher" action="" method="get">
@@ -54,17 +54,17 @@ if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 					<label for="language-switcher-locales">
 						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
 						<span class="screen-reader-text">
-							Ngôn ngữ						</span>
+							Language						</span>
 					</label>
 
 					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
-<option value="en_GB" lang="en" data-installed="1">English (UK)</option>
-<option value="vi" lang="vi" selected data-installed="1">Tiếng Việt</option></select>
+<option value="en_GB" lang="en" selected data-installed="1">English (UK)</option>
+<option value="vi" lang="vi" data-installed="1">Tiếng Việt</option></select>
 					
-											<input type="hidden" name="redirect_to" value="http://localhost/doancuoiki/wp-admin/?simply_static_page=47">
+											<input type="hidden" name="redirect_to" value="http://localhost/doancuoiki/wp-admin/?simply_static_page=800">
 					
 					
-						<input type="submit" class="button" value="Thay đổi">
+						<input type="submit" class="button" value="Change">
 
 					</form>
 				</div>
@@ -80,22 +80,22 @@ if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
 <script id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
-<script id="password-strength-meter-js-extra">var pwsL10n = {"unknown":"Mật khẩu mạnh không xác định","short":"Rất yếu","bad":"Yếu","good":"Trung bình","strong":"Mạnh","mismatch":"Mật khẩu không khớp"};</script>
+<script id="password-strength-meter-js-extra">var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};</script>
 <script id="password-strength-meter-js-translations">( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
-} )( "default", {"translation-revision-date":"2022-08-03 11:30:24+0000","generator":"GlotPress\/4.0.0-alpha.4","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"vi_VN"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s đã ngừng hoạt động từ phiên bản %2$s! Sử dụng thay thế bằng %3$s."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );</script>
+} )( "default", {"translation-revision-date":"2023-04-19 10:51:23+0000","generator":"GlotPress\/4.0.0-alpha.4","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"en_GB"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );</script>
 <script src="https://yeshope.github.io/doancuoiki/wp-admin/js/password-strength-meter.min.js?ver=6.2.2" id="password-strength-meter-js"></script>
 <script src="https://yeshope.github.io/doancuoiki/wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
 <script id="wp-util-js-extra">var _wpUtilSettings = {"ajax":{"url":"\/doancuoiki\/wp-admin\/admin-ajax.php"}};</script>
 <script src="https://yeshope.github.io/doancuoiki/wp-includes/js/wp-util.min.js?ver=6.2.2" id="wp-util-js"></script>
-<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"35c8db5bbe"};</script>
+<script id="user-profile-js-extra">var userProfileL10n = {"user_id":"0","nonce":"adb434b42c"};</script>
 <script id="user-profile-js-translations">( function( domain, translations ) {
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
-} )( "default", {"translation-revision-date":"2022-08-03 11:30:24+0000","generator":"GlotPress\/4.0.0-alpha.4","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"vi_VN"},"Your new password has not been saved.":["Mật khẩu mới của bạn chưa được lưu."],"Hide":["Ẩn đi"],"Show":["Hiển thị"],"Confirm use of weak password":["Chấp nhận sử dụng mật khẩu yếu."],"Hide password":["Ẩn mật khẩu"],"Show password":["Hiện mật khẩu"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );</script>
+} )( "default", {"translation-revision-date":"2023-04-19 10:51:23+0000","generator":"GlotPress\/4.0.0-alpha.4","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"en_GB"},"Your new password has not been saved.":["Your new password has not been saved."],"Show":["Show"],"Hide":["Hide"],"Confirm use of weak password":["Confirm use of weak password"],"Hide password":["Hide password"],"Show password":["Show password"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );</script>
 <script src="https://yeshope.github.io/doancuoiki/wp-admin/js/user-profile.min.js?ver=6.2.2" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
